275075ce33ab4c6ffaa1d3420834e295cf683bdb
jschauer1 <jschauer1@mines.edu>
2022-09-18 15:44:41 -0600

    Updated Toolbox

--- Scripts/Toolbox.cs
+++ Scripts/Toolbox.cs
@@ -4,64 +4,70 @@ using UnityEngine;
 
 public class Toolbox : MonoBehaviour
 {
-    public float StartTimeBetweenShots;
-    private float TimeBtwShots;
-    public GameObject pBullet;
-    public Transform bullet;
-    public GameObject[] pEnemy;
-    public Transform Enemy;
-    public int p;
-    public SpawnEnemy Script;
-    public int r;
-    public float listexceptiontimer;
-    public GameObject closestEnemy;
-    public Transform pCommander;
-    public Transform EnemyBoss;
+    //paste in and Control Shift / to uncomment.
+    /* Transform ObjectReference(string Reference)
+     {
+         if (GameObject.Find(Reference) != null)
+         {
+             Transform Object = GameObject.Find(Reference).transform;
+             return Object;
+         }
+         return null;
 
+     }
+     /*    GameObject GameObjectReference(string Reference)
+         {
+             if (GameObject.Find(Reference) != null)
+             {
+                 GameObject Object = GameObject.Find(Reference);
+                 return Object;
+             }
+             return null;
+         }*/
+    /*    int count(string ObjectCounted)
+        {
+            GameObject[] counts;
+            counts = GameObject.FindGameObjectsWithTag(ObjectCounted);
+            return counts.Length;
+        }*/
+    /*    Vector2 random(float x1, float x2, float y1, float y2)
+        {
+            Vector2 randoms;
+            randoms = new Vector2(Random.Range(x1, x2), Random.Range(y1, y2));
+            return randoms;
+        }*/
+    /*    bool ifclose(float x, float y)
+        {
+            float radiusx;
+            float radiusy;
+            Transform pCommander = ObjectReference("Commander");
+            Transform closestEnemy = FindClosestEnemy("EnemyBug(Clone)").transform;
+            radiusx = Mathf.Abs(pCommander.position.x - closestEnemy.position.x);
+            radiusy = Mathf.Abs(pCommander.position.y - closestEnemy.position.y);
+            if (radiusx < x && radiusy < y)
+            {
+                return true;
+            }
+            return false;
+        }*/
+    /*    GameObject FindClosestEnemy(string CloseEnemy)
+    {
+        GameObject[] gos;
+        gos = GameObject.FindGameObjectsWithTag(CloseEnemy);
+        GameObject closest = null;
+        float distance = Mathf.Infinity;
+        Vector3 position = transform.position;
+        foreach (GameObject go in gos)
+        {
+            Vector3 diff = go.transform.position - position;
+            float curDistance = diff.sqrMagnitude;
+            if (curDistance < distance)
+            {
+                closest = go;
+                distance = curDistance;
+            }
+        }
+        return closest;
+    }*/
 
-    Transform ObjectReference(string Reference)
-    {
-        if (GameObject.Find(Reference) != null)
-        {
-            Transform Object = GameObject.Find(Reference).transform;
-            return Object;
-        }
-        return null;
-
-    }
-    GameObject GameObjectReference(string Reference)
-    {
-        if (GameObject.Find(Reference) != null)
-        {
-            GameObject Object = GameObject.Find(Reference);
-            return Object;
-        }
-        return null;
-    }
-    int count(string ObjectCounted)
-    {
-        GameObject[] counts;
-        counts = GameObject.FindGameObjectsWithTag(ObjectCounted);
-        return counts.Length;
-    }
-    Vector2 random(float x1, float x2, float y1, float y2)
-    {
-        Vector2 randoms;
-        randoms = new Vector2(Random.Range(x1, x2), Random.Range(y1, y2));
-        return randoms;
-    }
-    bool ifclose(float x, float y)
-    {
-        float radiusx;
-        float radiusy;
-        //closestEnemy = FindClosestEnemy("EnemyBug(Clone)");
-        Enemy = closestEnemy.transform;
-        radiusx = Mathf.Abs(pCommander.position.x - Enemy.position.x);
-        radiusy = Mathf.Abs(pCommander.position.y - Enemy.position.y);
-        if (radiusx < x && radiusy < y)
-        {
-            return true;
-        }
-        return false;
-    }
 }
